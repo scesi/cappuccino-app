25ac304ca85bc22bbc22938fd9e4f799d0d4c5b4
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2024-07-09 15:46:35 -0400

    Add a11y eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
