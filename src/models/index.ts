c6bd17deca44b69706236aaeb6ae39efb7f0d4aa
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-07-11 23:29:31 -0300

    feat: add barril file in models

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+export * from "./career.interface";
+export * from "./day.enum";
+export * from "./group.interface";
+export * from "./level.interface";
+export * from "./schedule.interface";
+export * from "./subject.interface";
