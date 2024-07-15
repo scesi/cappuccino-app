ddc8550236115cb2d5c59e90614b049663f8d2f4
Herlan <herlanherbas@gmail.com>
2024-07-15 14:22:32 -0400

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ScheduleTable from './components/ScheduleTable'
+import { Day, Schedule } from './models'
 
+const sampleSchedule: Schedule[] = [
+  {
+    day: Day.Lu,
+    start: '1200',
+    end: '1415',
+    duration: 3,
+    room: '608A',
+    isClass: false,
+    teacher: 'FERNANDEZ TERRAZAS ERIKA',
+  },
+  {
+    day: Day.Ma,
+    start: '1700',
+    end: '1030',
+    duration: 3,
+    room: '614',
+    isClass: false,
+    teacher: 'ALFARO DENUS GONZALO',
+  },
+  {
+    day: Day.Mi,
+    start: '0800',
+    end: '1030',
+    duration: 3,
+    room: '614',
+    isClass: false,
+    teacher: 'ALFARO DENUS GONZALO',
+  },
+]
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>Schedule</h1>
+        <ScheduleTable schedule={sampleSchedule} />
       </div>
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
