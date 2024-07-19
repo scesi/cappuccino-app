d27d18151d53dc68eadbd9a5307be8222cdbcd32
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2024-07-18 20:10:04 -0400

    Feat: header component with a threeDots component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import ThreeDots from '../Three-dots/Three-dots'
+
+import logoSCESI from '../../assets/logoSCESI-Normal.svg'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <img className="logoscesi" src={logoSCESI} alt="logo scesi" />
+      <h1 className='title'>CAPPUCHINO</h1>
+      <ThreeDots></ThreeDots>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Three-dots/Three-dots.tsx
@@ -0,0 +1,13 @@
+import './Three-dots.css'
+
+const ThreeDots = () => {
+  return (
+    <div className="threedots">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  )
+}
+
+export default ThreeDots
