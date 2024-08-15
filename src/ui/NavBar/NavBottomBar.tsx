10cf8ae012660b0252df10f2a35da70562508348
IVANCCAMA <ccamaivan1@gmail.com>
2024-08-15 16:09:33 -0400

    Update Svg Icons

--- src/ui/NavBar/NavBottomBar.tsx
+++ src/ui/NavBar/NavBottomBar.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import IconCarreras from '../../assets/Icons/icon-carreras.png'
-import IconHorario from '../../assets/Icons/icon-horario.png'
-import IconMas from '../../assets/Icons/icon-mas.png'
-import IconHorarioActive from '../../assets/Icons/icon-horario-active.png'
-import IconMasActive from '../../assets/Icons/icon-mas-active.png'
 import './NavBottomBar.css'
+
+import SvgCareers from './IconsSvg/careers.svg'
+import SvgSchedules from './IconsSvg/schedules.svg'
+import SvgMore from './IconsSvg/more.svg'
+import SvgSchedulesActive from './IconsSvg/schedules-active.svg'
+import SvgMoreActive from './IconsSvg/more-active.svg'
 
 const NavBar = () => {
   const [activeButtonIndex, setActiveButtonIndex] = useState<number | null>(
@@ -22,7 +23,7 @@ const NavBar = () => {
           className={`button-icon ${activeButtonIndex === 0 ? 'active' : ''}`}
           onClick={() => handleButtonClick(0)}
         >
-          <img src={IconCarreras} alt="icon description" />
+          <img src={SvgCareers} alt="carreras option" />
           <span>Carreras</span>
         </button>
         <button
@@ -30,7 +31,7 @@ const NavBar = () => {
           onClick={() => handleButtonClick(1)}
         >
           <img
-            src={activeButtonIndex === 1 ? IconHorarioActive : IconHorario}
+            src={activeButtonIndex === 1 ? SvgSchedulesActive : SvgSchedules}
             alt="icon description"
           />
           <span>Horario</span>
@@ -40,7 +41,7 @@ const NavBar = () => {
           onClick={() => handleButtonClick(2)}
         >
           <img
-            src={activeButtonIndex === 2 ? IconMasActive : IconMas}
+            src={activeButtonIndex === 2 ? SvgMoreActive : SvgMore}
             alt="icon description"
           />
           <span>M&aacute;s</span>
