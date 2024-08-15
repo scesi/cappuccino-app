642b02752a9dc7230599d10248e4e1c20c2650f3
Herlan <herlanherbas@gmail.com>
2024-08-15 19:38:28 -0400

    feat: Add UI components for schedule table and utilities for parsing and formatting time

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,6 @@
+export * from './Button/index';
+export * from './schedule-table/schedule-table';
+export * from './schedule-table/utils/day-parser';
+export * from './schedule-table/utils/schedule-combiner';
+export * from './schedule-table/utils/schedule-utils';
+export * from './schedule-table/utils/time-formatter';
