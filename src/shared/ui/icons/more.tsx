499d51f37672008b931ddbf6c9544dbf3860405a
IVANCCAMA <ccamaivan1@gmail.com>
2024-08-15 22:55:17 -0400

    Refactor: moved files icon and convert svg to component tsx

---
+++ src/shared/ui/icons/more.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from 'react'
+
+type Props = SVGProps<SVGSVGElement>
+
+export const CoffeIcon = (props: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#000"
+      d="M5 2h2v3H5V2Zm4 0h2v3H9V2Zm4 0h2v3h-2V2Zm6 7h-2V7H3v11c0 1.654 1.346 3 3 3h8c1.654 0 3-1.346 3-3h2c1.103 0 2-.897 2-2v-5c0-1.103-.897-2-2-2Zm-4 9a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9h10v9Zm2-2v-5h2l.002 5H17Z"
+    />
+  </svg>
+)
---
+++ src/shared/ui/icons/trash.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from 'react'
+
+type Props = SVGProps<SVGSVGElement>
+
+export const TrashIcon = (props: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={18}
+    height={18}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#000"
+      d="M3.75 15a1.5 1.5 0 0 0 1.5 1.5h7.5a1.5 1.5 0 0 0 1.5-1.5V6h1.5V4.5h-3V3a1.5 1.5 0 0 0-1.5-1.5h-4.5A1.5 1.5 0 0 0 5.25 3v1.5h-3V6h1.5v9Zm3-12h4.5v1.5h-4.5V3ZM6 6h6.75v9h-7.5V6H6Z"
+    />
+    <path fill="#000" d="M6.75 7.5h1.5v6h-1.5v-6Zm3 0h1.5v6h-1.5v-6Z" />
+  </svg>
+)
