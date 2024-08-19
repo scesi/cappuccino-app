21ec371588d39ceaa8fbccf82b6c9dd627b42993
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-08-19 13:57:26 -0300

    fix: accordion styles

--- src/shared/ui/accordion/accordion-summary/accordion-summary.tsx
+++ src/shared/ui/accordion/accordion-summary/accordion-summary.tsx
@@ -1,20 +1,16 @@
-import { useContext } from 'react'
+import { ReactNode, useContext } from 'react'
 import { AccordionContext } from '@/shared/ui/accordion/accordion-context'
 
 import styles from './accordion-summary.module.css'
 
-export function AccordionSummary({ textValue }: { textValue: string }) {
+export function AccordionSummary({ children }: { children: ReactNode }) {
   const { toggle } = useContext(AccordionContext)
 
   return (
-    <section
-      className={styles.accordionSummary}
-      style={{
-        margin: 0,
-        padding: 0,
-      }}
-    >
-      <button className={styles.accordionSummaryLabel} onClick={toggle}>{textValue}</button>
+    <section className={styles.accordionSummary}>
+      <button className={styles.accordionSummaryLabel} onClick={toggle}>
+        {children}
+      </button>
     </section>
   )
 }
