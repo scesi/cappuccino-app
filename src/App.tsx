2c23de8b376992723a3bf05667cc1dd7cfbba73e
IVANCCAMA <ccamaivan1@gmail.com>
2024-08-20 19:03:23 -0400

    Refactor reaname to navbar-options-menu folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import { NavBar } from './ui/navbar/nav-bottom-bar'
+import { NavBar } from './ui/nav-buttom-bar/nav-bottom-bar'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
