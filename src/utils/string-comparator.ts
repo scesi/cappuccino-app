57e91deef41ab99955c9cfa776a4d21101f131d1
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2024-09-08 23:05:35 -0400

    Refactor isTextSimilarToText method logic

--- src/utils/string-comparator.ts
+++ src/utils/string-comparator.ts
@@ -2,8 +2,5 @@ export const isTextSimilarToText = (
   firstText: string,
   secondText: string,
 ): boolean => {
-  return firstText
-    .toString()
-    .toUpperCase()
-    .includes(secondText.toString().toUpperCase())
+  return firstText.toUpperCase().includes(secondText.toUpperCase())
 }
