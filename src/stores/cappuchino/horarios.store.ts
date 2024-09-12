269e11858bc9fa3a4476f6b62a7a7e2649675277
Nahuel <nahueltorrezg@gmail.com>
2024-09-12 17:49:45 -0400

    Update cappuchino store

--- src/stores/cappuchino/horarios.store.ts
+++ src/stores/cappuchino/horarios.store.ts
@@ -1,23 +1,28 @@
-import { create } from 'zustand';
-import { GroupScheduleItem, Day, SubjectGroup } from './students.store.ts';
+import { create } from 'zustand'
+import {
+  GroupScheduleItem,
+  Day,
+  SubjectGroup,
+} from './../../models/degree-program.ts'
 
 interface HorarioState {
-  horario: { [key in Day]?: GroupScheduleItem[] }; 
-  actualizarHorario: (group: SubjectGroup) => void;
-  limpiarHorario: () => void;
+  horario: { [key in Day]?: GroupScheduleItem[] }
+  actualizarHorario: (group: SubjectGroup) => void
+  limpiarHorario: () => void
 }
 
 export const useHorarioStore = create<HorarioState>((set) => ({
-  horario: {},  
+  horario: {},
 
-  actualizarHorario: (group: SubjectGroup) => set((state) => {
-    const nuevoHorario = { ...state.horario };
-    group.schedule.forEach((item) => {
-      if (!nuevoHorario[item.day]) nuevoHorario[item.day] = [];
-      nuevoHorario[item.day]!.push(item);
-    });
-    return { horario: nuevoHorario };
-  }),
+  actualizarHorario: (group: SubjectGroup) =>
+    set((state) => {
+      const nuevoHorario = { ...state.horario }
+      group.schedule.forEach((item) => {
+        if (!nuevoHorario[item.day]) nuevoHorario[item.day] = []
+        nuevoHorario[item.day]!.push(item)
+      })
+      return { horario: nuevoHorario }
+    }),
 
   limpiarHorario: () => set({ horario: {} }),
-}));
+}))
--- src/stores/cappuchino/students.store.ts
+++ src/stores/cappuchino/students.store.ts
@@ -1,85 +1,61 @@
-import { create, StateCreator } from 'zustand';
-import { persist, PersistOptions } from 'zustand/middleware';
-
-export interface GroupScheduleItem {
-  day: Day;
-  startHour: string;
-  endHour: string;
-  classroomName: string;
-  isRegularClass: boolean;
-  _duration: number;
-  _teacherName: string;
-}
-
-export interface SubjectGroup {
-  code: string;
-  teacherName: string;
-  schedule: GroupScheduleItem[];
-}
-
-export interface Subject {
-  id: string;
-  name: string;
-  groups: SubjectGroup[];
-}
-
-export interface Semester {
-  id: string;
-  label: string;
-  subjects: Subject[];
-}
-
-export interface DegreeProgram {
-  id: string;
-  name: string;
-  semesters: Semester[];
-}
-
-export enum Day {
-  Lu = 'LU',
-  Ma = 'MA',
-  Mi = 'MI',
-  Ju = 'JU',
-  Vi = 'VI',
-  Sa = 'SA',
-}
+import { create, StateCreator } from 'zustand'
+import { persist, PersistOptions } from 'zustand/middleware'
+import {
+  Semester,
+  DegreeProgram,
+  Subject,
+  SubjectGroup,
+} from './../../models/degree-program.ts'
 
 interface OrganizadorState {
-  degreePrograms: DegreeProgram[];
-  selectedDegreeProgram: DegreeProgram | null;
-  selectedSemester: Semester | null;
-  selectedSubject: Subject | null;
-  selectedSubjectGroup: SubjectGroup | null;
+  degreePrograms: DegreeProgram[]
+  selectedDegreeProgram: DegreeProgram | null
+  selectedSemester: Semester | null
+  selectedSubject: Subject | null
+  selectedSubjectGroup: SubjectGroup | null
 
-  setDegreePrograms: (programs: DegreeProgram[]) => void;
-  selectDegreeProgram: (program: DegreeProgram) => void;
-  selectSemester: (semester: Semester) => void;
-  selectSubject: (subject: Subject) => void;
-  selectSubjectGroup: (group: SubjectGroup) => void;
+  setDegreePrograms: (programs: DegreeProgram[]) => void
+  selectDegreeProgram: (program: DegreeProgram) => void
+  selectSemester: (semester: Semester) => void
+  selectSubject: (subject: Subject) => void
+  selectSubjectGroup: (group: SubjectGroup) => void
 }
 
 type MyPersist = (
   config: StateCreator<OrganizadorState>,
-  options: PersistOptions<OrganizadorState>
-) => StateCreator<OrganizadorState>;
+  options: PersistOptions<OrganizadorState>,
+) => StateCreator<OrganizadorState>
 
 export const useOrganizadorStore = create<OrganizadorState>(
   (persist as MyPersist)(
     (set) => ({
-      degreePrograms: [],  
+      degreePrograms: [],
       selectedDegreeProgram: null,
       selectedSemester: null,
       selectedSubject: null,
       selectedSubjectGroup: null,
 
       setDegreePrograms: (programs) => set({ degreePrograms: programs }),
-      selectDegreeProgram: (program) => set(() => ({ selectedDegreeProgram: program, selectedSemester: null, selectedSubject: null, selectedSubjectGroup: null })),
-      selectSemester: (semester) => set(() => ({ selectedSemester: semester, selectedSubject: null, selectedSubjectGroup: null })),
-      selectSubject: (subject) => set(() => ({ selectedSubject: subject, selectedSubjectGroup: null })),
-      selectSubjectGroup: (group) => set(() => ({ selectedSubjectGroup: group })),
+      selectDegreeProgram: (program) =>
+        set(() => ({
+          selectedDegreeProgram: program,
+          selectedSemester: null,
+          selectedSubject: null,
+          selectedSubjectGroup: null,
+        })),
+      selectSemester: (semester) =>
+        set(() => ({
+          selectedSemester: semester,
+          selectedSubject: null,
+          selectedSubjectGroup: null,
+        })),
+      selectSubject: (subject) =>
+        set(() => ({ selectedSubject: subject, selectedSubjectGroup: null })),
+      selectSubjectGroup: (group) =>
+        set(() => ({ selectedSubjectGroup: group })),
     }),
     {
-      name: 'organizador-storage',  
-    }
-  )
-);
+      name: 'organizador-storage',
+    },
+  ),
+)
