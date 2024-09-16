d79cea254a25c0678560fb442b06f0439614d8df
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2024-09-16 17:06:54 -0400

    docs string-utils methods

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,3 +1,20 @@
+/**
+ * Method to check if one string is similar to another text string, ignoring case sensitivity
+ *
+ * @param {string} firstText - String in which to search for the presence of secondText
+ * @param {string} secondText - String to search for within firstText
+ * @returns {boolean} - true if secondText is found within firstText, otherwise false
+ *
+ * @example
+ * // Example of checking if the text 'wORLD' is found within 'Hello World'
+ * text1 = 'Hello World'
+ * text2 = 'wORLD'
+ * result = isTextSimilarToText(text1, text2)
+ *
+ * console.log(isTextSimilarToText(text1, text2))
+ * // output: true
+ */
+
 export const isTextSimilarToText = (
   firstText: string,
   secondText: string,
