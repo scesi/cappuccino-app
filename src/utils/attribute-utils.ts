03cbc67983d62ed6fe9d9721fa431cf201aed8ac
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2024-09-16 17:07:52 -0400

    docs attribute-utils methods

--- src/utils/attribute-utils.ts
+++ src/utils/attribute-utils.ts
@@ -1,15 +1,19 @@
 /**
- * Method to flat a list of items in base of an atribute of the items list
+ * Method to flatten a list of items based on an attribute of the items list.
+ *
  * @param items - list of objects
  * @param attribute - existing attribute in the object array
  * @return List of objects contained within each attribute item
  *
- * Use examples:
- * items= [ {id: 1, name: 'Alberto', ci: { number: '115588447', department: 'CBBA' }},
+ * @example
+ * // Example of flattening objects in the items list where the determining attribute is 'ci'.
+ * items = [ {id: 1, name: 'Alberto', ci: { number: '115588447', department: 'CBBA' }},
  *          {id: 2, name: 'Luisa', ci: { number: '885577449', department: 'CBBA' }}, ]
  * attribute = 'ci'
- * Output: [ { number: '115588447', department: 'CBBA' },
- *          {id: 2, name: 'Luisa', ci: { number: '885577449', department: 'CBBA' }} ]
+ *
+ * console.log(flattenAttributes(items, attribute))
+ * // output: [ { number: '115588447', department: 'CBBA' },
+ * //           {id: 2, name: 'Luisa', ci: { number: '885577449', department: 'CBBA' }} ]
  */
 
 export const flattenAttributes = <T extends object>(
