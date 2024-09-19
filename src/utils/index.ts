7093a84a7b61722e7bd326892d997476eed37c62
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2024-09-18 23:43:42 -0400

    Refactor string-utils file name changed to comparator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export * from './attribute-utils'
 export * from './filter-utils'
 export * from './object-level-flattener-utils'
-export * from './string-utils'
+export * from './string-utils/comparator'
